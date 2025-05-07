703f88dfe6eda491256900fc4f27aba54cfc247d
cccshun <caishun_1@163.com>
2025-05-07 21:02:03 +0800

    Add observer pattern

---
+++ src/main/java/com/sysu/observer/ObserverDemo.java
@@ -0,0 +1,18 @@
+package com.sysu.observer;
+
+import java.util.Observable;
+
+public class ObserverDemo {
+    public static void main(String[] args) {
+        Boss boss = new Boss();
+        NBAObserver nbaObserver = new NBAObserver("Obama", boss);
+        StockObserver stockObserver = new StockObserver("Trump", boss);
+
+        boss.attach(nbaObserver);
+        boss.attach(stockObserver);
+
+        System.out.println(boss.getObserverList());
+        boss.setState("huhansan is coming");
+        boss.notifyObservers();
+    }
+}
